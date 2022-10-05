d17e9571cbbea3e18f10826afbf9736876eb5e74
blueoasis <blueoasis@users.noreply.github.com>
2022-10-05 12:18:51 -0600

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,15 @@
-import logo from './logo.svg';
-import './App.css';
+import "./styles.css";
+import { photosData } from "./constants/data";
+import Card from "./components/Card/Card";
 
-function App() {
+export default function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className="cardsContainer">
+        {photosData.map(({ id, title, thumbnailUrl }) => (
+          <Card id={id} key={id} title={title} thumbnailUrl={thumbnailUrl} />
+        ))}
+      </div>
     </div>
   );
 }
-
-export default App;
